700a817b2bc7da9070682b88e496ebc5e7bc4951
Alejandro Villarroel <84481926+Alejandro-VH@users.noreply.github.com>
2025-08-03 00:12:24 -0400

    feat: implement authentication service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'http://127.0.0.1:8000/api'
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    const body = { email, password };
+    return this.http.post(`${this.baseUrl}/login`, body);
+  }
+
+  register(name: string, email: string, password: string) {
+    const body = { name, email, password };
+    return this.http.post(`${this.baseUrl}/register`, body);
+  }
+}
