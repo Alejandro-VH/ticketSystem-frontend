97263fa38ab35330772ca53948f52327c6ace251
Alejandro Villarroel <84481926+Alejandro-VH@users.noreply.github.com>
2025-08-11 00:36:15 -0400

    feat: update routing and base configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(FormsModule, HttpClientModule)
+    importProvidersFrom(FormsModule, HttpClientModule),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component'
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
+import { SupportDashboardComponent } from './pages/support/support-dashboard/support-dashboard.component';
+import { UserHomeComponent } from './pages/user/user-home/user-home.component';
+import { CreateTicketComponent } from './pages/user/create-ticket/create-ticket.component';
+import { TicketDetailComponent } from './shared/tickets/ticket-detail/ticket-detail.component';
 
-export const routes: Routes = [];
+import { authGuard } from './guards/auth.guard';
+import { adminGuard } from './guards/admin.guard';
+import { supportGuard } from './guards/support.guard';
+import { userGuard } from './guards/user.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { EditTicketComponent } from './pages/support/edit-ticket/edit-ticket.component';
+import { EditUserComponent } from './pages/admin/edit-user/edit-user.component';
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent, canActivate: [authGuard]},
+    //{path: 'admin', component: AdminDashboardComponent,canActivate: [authGuard, adminGuard]},
+    //{path: 'support', component: SupportDashboardComponent,canActivate: [authGuard, supportGuard]},
+    //{path: 'user', component: UserHomeComponent,canActivate: [authGuard, userGuard]},
+    {path: 'user/create-ticket', component: CreateTicketComponent, canActivate: [authGuard, userGuard]},
+    {path: 'ticket/:id', component: TicketDetailComponent, canActivate: [authGuard]},
+    {path: 'ticket/edit/:id', component: EditTicketComponent, canActivate: [authGuard, supportGuard]},
+    {path: 'user/edit/:id', component: EditUserComponent, canActivate: [authGuard, adminGuard]},
+
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {path: '**', redirectTo: 'login'},
+
+];
